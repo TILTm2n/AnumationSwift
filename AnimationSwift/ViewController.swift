7067deb6aa61023d53c5f7fa8d42355726a01503
Eugene <evgen29031999@bk.ru>
2021-12-06 16:56:02 +0300

    add UIViewPropertyAnimator

--- AnimationSwift/ViewController.swift
+++ AnimationSwift/ViewController.swift
@@ -11,44 +11,22 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var viewForAnimation: UIView!
     
-    @IBOutlet weak var viewCenterYConstraint: NSLayoutConstraint!
-    
+    var animator: UIViewPropertyAnimator!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        UIView.animate(withDuration: 2.0){
-//            self.viewForAnimation.center = self.view.center
-//            self.viewForAnimation.alpha = 0.0
-//        }
+        viewForAnimation.center = view.center
         
-        self.viewForAnimation.frame.origin.x = 0
+        animator = UIViewPropertyAnimator(duration: 1.5, curve: .easeInOut, animations: {
+            self.viewForAnimation.transform = .init(scaleX: 1.5, y: 1.5)
+            self.viewForAnimation.backgroundColor = .red
+        })
         
-//        UIView.animateKeyframes(withDuration: 1.0, delay: 0.2, options: [.repeat, .autoreverse], animations: {
-//            self.viewForAnimation.frame.origin.x = self.view.bounds.maxX - self.viewForAnimation.bounds.width
-//
-//            UIView.addKeyframe(withRelativeStartTime: 0.2, relativeDuration: 0.3) {
-//                self.viewForAnimation.backgroundColor = .red
-//            }
-//
-//            UIView.addKeyframe(withRelativeStartTime: 0.1, relativeDuration: 0.5) {
-//                self.viewForAnimation.alpha = 0.5
-//                self.viewForAnimation.transform = .init(rotationAngle: .pi)
-//            }
-//
-//        }, completion: nil)
-        
-        UIView.animate(withDuration: 2.5,
-                       delay: 0.1,
-                       usingSpringWithDamping: 1.0,
-                       initialSpringVelocity: 2.0,
-                       options: [.curveLinear, .curveEaseInOut],
-                       animations: {
-            self.viewForAnimation.frame.origin.x =
-            self.view.bounds.maxX - self.viewForAnimation.bounds.width
-        },
-                       completion: nil)
-        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        animator.startAnimation()
     }
 
 
