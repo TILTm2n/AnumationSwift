f033608c8bf2a5de09cec41560d1b3baf72bd409
Eugene <evgen29031999@bk.ru>
2021-12-06 11:45:52 +0300

    identity

--- AnimationSwift/ViewController.swift
+++ AnimationSwift/ViewController.swift
@@ -19,13 +19,19 @@ class ViewController: UIViewController {
 //            self.viewForAnimation.alpha = 0.0
 //        }
         
-        UIView.animate(withDuration: 1.0, animations: {
+        UIView.animate(withDuration: 3.0, animations: {
             self.viewForAnimation.center = self.view.center
             
-            self.viewForAnimation.transform = CGAffineTransform(rotationAngle: .pi)
+            self.viewForAnimation.transform = CGAffineTransform(rotationAngle: -.pi/2).scaledBy(x: 2.5, y: 1.75).translatedBy(x: -30, y: 0)
             
         }) { (isFinish) in
             //self.viewForAnimation.alpha = 0.0 // срабатывает когда завершится анимация
+            
+            UIView.animate(withDuration: 1.0) {
+                self.viewForAnimation.transform = .identity
+            }
+            
+            
         }
     }
 
