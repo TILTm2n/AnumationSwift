3f177c4846c6586e1999cb355128443cdb582c89
Eugene <evgen29031999@bk.ru>
2021-12-06 18:22:08 +0300

    custom gesture

--- AnimationSwift/ViewController.swift
+++ AnimationSwift/ViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var animator: UIViewPropertyAnimator!
+    
     @IBOutlet weak var viewForAnimation: UIView!
     
-    var animator: UIViewPropertyAnimator!
+    @IBAction func didSlide(_ sender: UISlider) {
+        animator.fractionComplete = CGFloat(sender.value)/100
+    }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,16 +25,30 @@ class ViewController: UIViewController {
         viewForAnimation.center = view.center
         
         animator = UIViewPropertyAnimator(duration: 1.5, curve: .easeInOut, animations: {
-            self.viewForAnimation.transform = .init(scaleX: 1.5, y: 1.5)
+            
             self.viewForAnimation.backgroundColor = .red
+            
+            self.viewForAnimation.layer.cornerRadius = 15
+            
+            self.viewForAnimation.transform = .init(rotationAngle: .pi/2).scaledBy(x: 1.5, y: 1.5)
+            //self.viewForAnimation.transform = .init(scaleX: 1.5, y: 1.5)
         })
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(didTap))
+        
+        viewForAnimation.addGestureRecognizer(tapGesture)
         
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        animator.startAnimation()
+        
     }
 
+    @objc
+    func didTap(){
+        
+    }
+    
 
 }
 
